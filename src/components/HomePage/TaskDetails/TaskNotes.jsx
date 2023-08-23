2c5adcc3e157e5821397d85cef579a02ed03c833
jameela12 <jameela.khalil.95@gmail.com>
2023-08-23 14:57:37 +0300

    worked on the note page - back end

--- src/components/HomePage/TaskDetails/TaskNotes.jsx
+++ src/components/HomePage/TaskDetails/TaskNotes.jsx
@@ -1,24 +1,39 @@
 import { useState } from 'react';
 
-
 export default function TaskNotes() {
-
     const [notes, setNotes] = useState('');
 
-    const handleNotesChange = (event) => {
-        setNotes(event.target.value);
+    const handleNotesChange = async (event) => {
+        const newNotes = event.target.value;
+        setNotes(newNotes);
+
+        // Send a request to the back-end to save the updated notes
+        const response = await fetch('/api/notes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ notes: newNotes })
+        });
+
+        // Handle the response
+        if (response.ok) {
+            console.log('Notes updated successfully');
+        } else {
+            console.error('An error occurred while updating the notes');
+        }
     };
 
     return (
         <>
-        <hr className="task-line"></hr>
+            <hr className="task-line"></hr>
             <div className="task-desc">
                 <div className="task-textarea">
                     <textarea
-                    className="form-control"
-                    placeholder="Notes"
-                    value={notes}
-                    onChange={handleNotesChange} />
+                        className="form-control"
+                        placeholder="Notes"
+                        value={notes}
+                        onChange={handleNotesChange} />
                 </div>
             </div>
         </>
